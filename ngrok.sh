d1a6dc08609e4212dd19a8c89ad1fbd375ca786e
hidagans <58388773+hidagans@users.noreply.github.com>
2023-07-05 18:23:26 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,39 +1,13 @@
 #!/bin/bash
-# ngrok-setup-script.sh
-#
-# Script to setup ngrok for ssh connection
-#
-# For Linux
-#
-# Requires wget, unzip, and screen installed
 
-echo 'Create and ngrok account at ngrok.com'
-
-echo 'Starting ngrok setup'
-
-# Make ngrok directory
 mkdir ~/ngrok
 cd ~/ngrok
 
-# Download ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 
-# Unzip ngrok
 unzip ngrok-stable-linux-amd64.zip
 
-# Authenticate with ngrok account
-echo 'Input your ngrok account token'
+echo 'TOKEN ISI DISINI YA ANJG'
 read TOKEN
 ./ngrok authtoken $TOKEN
-
-# create screen session
-screen -S ngrok
-
-# How to detach screen session
-echo 'Ctr + a + d to detach session'
-
-# Start ngrok server
 ./ngrok tcp 22
-
-# List screen sessions
-screen -list
